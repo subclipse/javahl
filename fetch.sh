06bea279f05a3c9306202c457f149232d82bf7d5
Mark Phippard <markphip@gmail.com>
2016-09-03 14:00:22 -0400

    Script for fetching the source code from upstream

---
+++ fetch.sh
@@ -0,0 +1,8 @@
+# Script to refresh source code from upstream tag
+VERSION=1.9.4
+rm -rf src/main/java
+rm -rf src/test/java
+svn export http://svn.apache.org/repos/asf/subversion/tags/$VERSION/subversion/bindings/javahl/src/ src/main/java
+svn export http://svn.apache.org/repos/asf/subversion/tags/$VERSION/subversion/bindings/javahl/tests/ src/test/java
+rm -rf src/main/java/org/tigris
+rm -rf src/test/java/org/tigris
